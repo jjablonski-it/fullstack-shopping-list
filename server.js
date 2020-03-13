f71b14aeb088680bcb25df1c435bcfd029c4e031
Jakub Jabłoński <jjablonski.it@gmail.com>
2020-03-13 23:09:38 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Initialize app
+const app = express();
+app.use(express.json());
+
+// Load config
+require("dotenv").config();
+const { URI, PORT } = process.env;
+
+// Routes
+app.use("/api/shoppingList", require("./routes/shoppingList"));
+
+if (URI)
+  mongoose
+    .connect(URI, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(console.log("MongoDB connected successfuly"))
+    .catch(err => console.log("Error: " + err));
+
+if (PORT) app.listen(PORT, console.log("App listening on port " + PORT));
