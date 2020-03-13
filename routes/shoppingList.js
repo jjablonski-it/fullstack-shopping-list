1aef143ce22c0c544437d05dbbd2c885d7b97e9a
Jakub Jabłoński <jjablonski.it@gmail.com>
2020-03-13 23:18:07 +0100

    Added basic dummy endpoints

--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -1,7 +1,23 @@
 const router = require("express").Router();
 
+// Get items
 router.get("/", (req, res) => {
   return res.json({ msg: "Get shopping list" });
+});
+
+// Add item
+router.post("/", (req, res) => {
+  return res.json({ msg: "Add shopping list item" });
+});
+
+// Toogle bought
+router.patch("/:id", (req, res) => {
+  return res.json({ msg: "Toogle bought" });
+});
+
+// Delete item
+router.patch("/:id", (req, res) => {
+  return res.json({ msg: "Delete item" });
 });
 
 module.exports = router;
